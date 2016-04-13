3b1e7682e1cefd1d4efe30b64ed85f15fbed6c60
AndrewNgKF <andrew.ctrl.alt.del@gmail.com>
2016-04-14 00:58:01 +0800

    No CSV connection yet
    JUst made the thing work without any connections to CSV, but outlets
    work

---
+++ PokeDex/PokeCell.swift
@@ -0,0 +1,29 @@
+//
+//  PokeCell.swift
+//  PokeDex
+//
+//  Created by IMAC on 13/4/16.
+//  Copyright © 2016 Andrew's Personal. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell (pokemon: Pokemon) {
+     self.pokemon = pokemon
+        
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+    
+}
